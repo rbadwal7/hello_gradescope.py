21bc0c769c82162471e0795f52f30a9e1a889e94
Ravinder Singh <r.singh1891@gmail.com>
2022-09-24 15:06:48 -0700

    complete

---
+++ hello_gradescope.py
@@ -0,0 +1,10 @@
+# Name: Ravinder Badwal
+# OSU Email: badwalr@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: 0 Gradescope Submission
+# Due Date: October 3, 2022
+# Description: This program is used to confirm that I am corrected connected to gradescope.
+
+
+def gradescope_test():
+    return 42
